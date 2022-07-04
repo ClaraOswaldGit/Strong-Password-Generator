f033f88e8cc16254cdf17a601946f086c1b051a6
Clara Oswald <71993557+ClaraOswaldGit@users.noreply.github.com>
2022-07-04 09:35:16 +0200

    Put code into single function

--- strongPassGen.py
+++ strongPassGen.py
@@ -9,21 +9,25 @@ import random
 # Set the character list for generating the password
 characters = "01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ&*(){}[]|/\?!@#$%^abcdefghijklmnopqrstuvwxyz"
 
-# Take the length of the password from the user
-password_length = int(input('Enter the length of the password: '))
 
 # Generate strong password of at least 12 charachter length. Prompt user to re-enter length if under 12 characters.
-while True: 
-    ## Set the minimum password length to 12 characters
-    if password_length <= 11:
-        ### Under 12 characters error message
-        print('Strong passwords must be at least 12 characters long.')
-        ### Prompt user to enter a higher lenght
-        password_length = int(input('Enter a higher length: '))
-    else:
-        # Generate the password
-        password = "".join(random.sample(characters, password_length))
-        # Print the generated password
-        print("Gernerated password: %s" %password)
-        break
+def passGen():
+    # Take the length of the password from the user
+    password_length = int(input('Enter the length of the password: '))
+    while True: 
+        ## Set the minimum password length to 12 characters
+        if password_length <= 11:
+            ### Under 12 characters error message
+            print('Strong passwords must be at least 12 characters long.')
+            ### Prompt user to enter a higher lenght
+            password_length = int(input('Enter a higher length: '))
+        else:
+            # Generate the password
+            password = "".join(random.sample(characters, password_length))
+            # Print the generated password
+            print("Gernerated password: %s" %password)
+            break
+
+passGen()
+
 
