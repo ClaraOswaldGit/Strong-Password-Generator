786da58c219b3133fd869715140df65fb5402173
Clara Oswald <71993557+ClaraOswaldGit@users.noreply.github.com>
2022-07-09 10:24:07 +0200

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from tkinter import *
+import strongPassGen
+
+
+# Create Window
+window = Tk()
+
+# Give title to window
+window.title("Strong Password Generator")
+
+# Window height
+window.configure(width=700, height=500)
